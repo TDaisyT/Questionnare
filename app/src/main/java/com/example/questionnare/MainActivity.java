bfb91d139e756a71a1cb237ff1d9eef329c09cd5
Varga Petra <vargapetra0123@gmail.com>
2023-10-31 12:57:28 +0100

    revering back to state before toolbar

--- app/src/main/java/com/example/questionnare/MainActivity.java
+++ app/src/main/java/com/example/questionnare/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.questionnare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView signInRedirect;
     private TextView loginRedirect;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
