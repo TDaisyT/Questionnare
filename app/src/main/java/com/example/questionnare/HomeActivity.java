2e19722846c6ad1b1f0421ddb7fbea529386790f
Varga Petra <vargapetra0123@gmail.com>
2023-11-01 13:06:33 +0100

    Add activity to handle home screen

---
+++ app/src/main/java/com/example/questionnare/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.questionnare;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+
+
+}
