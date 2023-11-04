eef48047642b319ac609e19e5f3e8ad98d4b9d81
Papp Gréta <papp.greta2@gmail.com>
2023-11-04 19:08:11 +0100

    Filled up the qa table with data

--- app/src/main/java/com/example/questionnare/DatabaseManager.java
+++ app/src/main/java/com/example/questionnare/DatabaseManager.java
@@ -84,4 +84,69 @@ public class DatabaseManager {
             Log.d("DatabaseManager","Error: The results weren't inserted into the database");
         }
     }
+
+
+    public Cursor queryDatabase(String query) {
+        return database.rawQuery(query, null);
+    }
+
+
+    public void deleteAllQA() {
+        database.delete(DatabaseHelper.TABLE_QA, null, null);
+    }
+
+    public boolean isQAInDatabase(String question) {
+        String[] columns = { DatabaseHelper.COL_ID };
+        String selection = DatabaseHelper.COL_QUESTION + " = ?";
+        String[] selectionArgs = { question };
+        Cursor cursor = database.query(DatabaseHelper.TABLE_QA, columns, selection, selectionArgs, null, null, null);
+        boolean qaExists = (cursor.getCount() > 0);
+        cursor.close();
+        return qaExists;
+    }
+    public void initializeQA() {
+        String[] questions = {
+                "What is your major?",
+                "Are you satisfied with the teaching staff and their teaching methods?",
+                "Are you satisfied with the equipment the university offers?",
+                "How likely are you to continue attending this college next year?",
+                "How likely are you to continue your studies with a master's degree?",
+                "How likely are you to complete a different major in our university?",
+                "How many hours a week do you work your part-time job?",
+                "Do you plan to work in a similiar field as your major?",
+                "How likely are you to recommend this college to your friends and family?",
+                "Do you think that your university created a safe environment for your studies?",
+                "What are your views on the extracurricular activities carried out in this college?",
+                "Where do you live during your university studies",
+                "What is your overall experience with your major in our university?",
+                "What is your overall experience with the university?",
+                "What do you think we should work on more to create a better university experience?"
+        };
+
+        String[][] answers = {
+                { "IT Programs", "Agriculture Programs", "Health Sciences Programs", "Humanities & Education Programs" },
+                { "Dissatisfied", "Neutral", "Satisfied", "Unsure" },
+                { "Dissatisfied", "Neutral", "Satisfied", "Unsure" },
+                { "Not at all likely", "Somewhat likely", "Extremely likely", "Unsure" },
+                { "Not at all likely", "Somewhat likely", "Extremely likely", "Unsure" },
+                { "Not at all likely", "Somewhat likely", "Extremely likely", "Unsure" },
+                { "I don’t have a job", "1-10 hours a week", "10-20 hours a week", "More than 20 hours a week" },
+                { "Definitely no", "Definitely yes", "Already working in a similiar field", "Unsure" },
+                { "Not at all likely", "Somewhat likely", "Extremely likely", "Unsure" },
+                { "Definitely no", "It could be more safe", "Definitely yes", "Unsure" },
+                { "They are too few", "They are just right", "They are too many", "Unsure" },
+                { "Dormitories", "Apartments", "Local to your university's city", "Prefer not to say" },
+                { "Dissatisfied", "Neutral", "Satisfied", "Unsure" },
+                { "Dissatisfied", "Neutral", "Satisfied", "Unsure" },
+                { "Teaching staff and their methods", "Dormitories' prices, locations and spaces", "University's buildings and equipments", "Extracurricular activities, out of school programs" },
+        };
+
+        for (int i = 0; i < questions.length; i++) {
+            if (!isQAInDatabase(questions[i])) {
+                addQA(questions[i], answers[i][0], answers[i][1], answers[i][2], answers[i][3]);
+            }
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/questionnare/HomeActivity.java
+++ app/src/main/java/com/example/questionnare/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.questionnare;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,28 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        DatabaseManager dbManager = new DatabaseManager(this);
+        dbManager.open();
+        dbManager.initializeQA();
+/*JUST FOR CHECKING:
+        Cursor cursor = dbManager.queryDatabase("SELECT * FROM " + DatabaseHelper.TABLE_QA);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int id = cursor.getInt(cursor.getColumnIndex(DatabaseHelper.COL_ID));
+                String question = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_QUESTION));
+                String answer1 = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_ANSWER1));
+                String answer2 = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_ANSWER2));
+                String answer3 = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_ANSWER3));
+                String answer4 = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_ANSWER4));
+                Log.d("DatabaseContent", "ID: " + id + ", Question: " + question);
+                Log.d("DatabaseContent", "Answer 1: " + answer1);
+                Log.d("DatabaseContent", "Answer 2: " + answer2);
+                Log.d("DatabaseContent", "Answer 3: " + answer3);
+                Log.d("DatabaseContent", "Answer 4: " + answer4);
+            }
+            cursor.close();
+        }*/
+        dbManager.close();
     }
 
 
