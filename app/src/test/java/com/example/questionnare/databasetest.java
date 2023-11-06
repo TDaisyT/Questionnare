a83af26fa97165259faded8d5219c809d187e85c
FktEniko <99762595+FktEniko@users.noreply.github.com>
2023-11-06 10:12:05 +0100

    databasetest írása.java

--- app/src/test/java/com/example/questionnare/databasetest.java
+++ app/src/test/java/com/example/questionnare/databasetest.java
@@ -1,1 +1,22 @@
+@RunWith(AndroidJUnit4.class)
+public class MyDatabaseTest {
+    private MyDatabase myDatabase;
 
+    @Before
+    public void initDb() {
+        myDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+                MyDatabase.class)
+                .build();
+    }
+
+    @After
+    public void closeDb() {
+        myDatabase.close();
+    }
+
+    @Test
+    public void insertAndReadData() {
+        // Adatok beszúrása az adatbázisba
+        myDatabase.myDao().insertData(new Data("example data"));
+
+        // Adat
