ffdf3981a2896c768980b04baa8c923a7d45b8d4
Varga Petra <vargapetra0123@gmail.com>
2023-11-07 12:22:57 +0100

    Add default fragment to HomeActivity

--- app/src/main/java/com/example/questionnare/HomeActivity.java
+++ app/src/main/java/com/example/questionnare/HomeActivity.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class HomeActivity extends AppCompatActivity {
-
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    Qlist mylist = new Qlist();
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        fragmentTransaction.replace(R.id.container, mylist);
+        fragmentTransaction.commit();
     }
 
 
