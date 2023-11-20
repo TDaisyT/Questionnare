c66707cb7634a7fc1c45aa1021ddac06fd7a270e
lucyrina <kaponyaslucy@gmail.com>
2023-11-20 11:03:15 +0100

    logout button works

--- app/src/main/java/com/example/questionnare/UserHome/ProfileActivity.java
+++ app/src/main/java/com/example/questionnare/UserHome/ProfileActivity.java
@@ -1,25 +1,42 @@
 package com.example.questionnare.UserHome;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.questionnare.HomeActivity;
+import com.example.questionnare.MainActivity;
 import com.example.questionnare.R;
+import com.example.questionnare.SignUpActivity;
 
 public class ProfileActivity extends Fragment {
 
-    //todo
-    // kijelentkezés gomb
+    private Button logOutButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        logOutButton = view.findViewById(R.id.logout);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
         //Kinyerem az Intentből az emailt és jelszót
         String email = getActivity().getIntent().getStringExtra("email");
         String password = getActivity().getIntent().getStringExtra("password");
