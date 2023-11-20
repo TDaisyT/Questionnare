fae5d8c3b8c97c96a0a5e89ba8d81d76b2ae5460
lucyrina <kaponyaslucy@gmail.com>
2023-11-20 15:46:58 +0100

    updated statistic page

--- app/src/main/java/com/example/questionnare/AdminHome/StatisticActivity.java
+++ app/src/main/java/com/example/questionnare/AdminHome/StatisticActivity.java
@@ -2,10 +2,14 @@ package com.example.questionnare.AdminHome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.questionnare.R;
+import com.github.mikephil.charting.charts.PieChart;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class StatisticActivity extends AppCompatActivity {
 
@@ -28,5 +32,20 @@ public class StatisticActivity extends AppCompatActivity {
         //  lényegében kimásolni a HomeActivityből a setOnItemSelectedListener
         //  csak az id ket kicserélni 'home' és 'profil' helyet 'barChartButton' re és 'pieCharButton'ra
         //  és átirni a megnyitandó képernyő nevét
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                if (item.getItemId() == R.id.barChartButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, barChartFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.pieChartButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, pieChartFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
