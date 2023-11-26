ca380e7da32293b24dc5668f14aef6e674270b9e
FktEniko <feniko78@gmail.com>
2023-11-26 10:24:07 +0100

    ExampleUnitTest.java change

--- app/src/test/java/com/example/questionnare/ExampleUnitTest.java
+++ app/src/test/java/com/example/questionnare/ExampleUnitTest.java
@@ -5,13 +5,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Unit test for validating basic arithmetic operations.
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        // Arrange
+        int operand1 = 2;
+        int operand2 = 2;
+
+        // Act
+        int result = ArithmeticUtil.add(operand1, operand2);
+
+        // Assert
+        assertEquals("The addition result is correct", 4, result);
     }
 }
