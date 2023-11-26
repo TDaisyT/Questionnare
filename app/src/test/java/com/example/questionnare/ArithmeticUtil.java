bcaed5bc3d3d8b9b8fb7bdf6c3dd1abe1ef63861
FktEniko <feniko78@gmail.com>
2023-11-26 10:22:30 +0100

    ArithmeticUtil.java update

--- app/src/test/java/com/example/questionnare/ArithmeticUtil.java
+++ app/src/test/java/com/example/questionnare/ArithmeticUtil.java
@@ -1,4 +1,19 @@
 package com.example.questionnare;
 
+/**
+ * Utility class for basic arithmetic operations.
+ */
 public class ArithmeticUtil {
+
+    /**
+     * Adds two integers.
+     *
+     * @param a The first operand.
+     * @param b The second operand.
+     * @return The sum of the operands.
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
+
