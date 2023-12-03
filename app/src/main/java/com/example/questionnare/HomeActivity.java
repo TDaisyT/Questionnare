f1386485336d58d6a845e3527043e37099ab4594
FktEniko <feniko78@gmail.com>
2023-12-03 02:59:21 +0100

    HomeActivity.java change

--- app/src/main/java/com/example/questionnare/HomeActivity.java
+++ app/src/main/java/com/example/questionnare/HomeActivity.java
@@ -1,26 +1,18 @@
 package com.example.questionnare;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.questionnare.Database.DatabaseManager;
 import com.example.questionnare.UserHome.ProfileActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
-    private FragmentManager fragmentManager = getSupportFragmentManager();
-    private FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    private Qlist questionnaireList = new Qlist();
-    private ProfileActivity profile = new ProfileActivity();
-
-    private BottomNavigationView bottomNavigationView;
-
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
+    private final Qlist questionnaireList = new Qlist();
+    private final ProfileActivity profile = new ProfileActivity();
 
 
     @Override
@@ -32,26 +24,22 @@ public class HomeActivity extends AppCompatActivity {
         dbManager.initializeQA();
         dbManager.close();
 
-        bottomNavigationView = findViewById(R.id.bottomNav);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
 
         //a home  képernyő defaultnak beállitja a kérdőivek listáját tartalmazó képernyőt (Qlist az activityje)
         getSupportFragmentManager().beginTransaction().replace(R.id.container, questionnaireList).commit();
 
         //jó gombhoz jó képernyőt hozzon be
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
 
-                if (item.getItemId() == R.id.home) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, questionnaireList).commit();
-                    return true;
-                } else if (item.getItemId() == R.id.profile) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profile).commit();
-                    return true;
-                }
-                return false;
+            if (item.getItemId() == R.id.home) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, questionnaireList).commit();
+                return true;
+            } else if (item.getItemId() == R.id.profile) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, profile).commit();
+                return true;
             }
+            return false;
         });
 
     }
