33362d7a0ff3a3908946fc023aa54acd9aed5138
FktEniko <feniko78@gmail.com>
2023-12-03 03:03:09 +0100

    StatisticActivity.java tisztítása

--- app/src/main/java/com/example/questionnare/AdminHome/StatisticActivity.java
+++ app/src/main/java/com/example/questionnare/AdminHome/StatisticActivity.java
@@ -2,14 +2,9 @@ package com.example.questionnare.AdminHome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.example.questionnare.R;
-import com.github.mikephil.charting.charts.PieChart;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class StatisticActivity extends AppCompatActivity {
 
