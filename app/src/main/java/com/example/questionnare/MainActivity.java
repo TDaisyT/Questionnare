bc6d43b6c30ae557422cc4808fa3d5331b00ea97
FktEniko <feniko78@gmail.com>
2023-12-03 02:56:47 +0100

    MainActivity.java change

--- app/src/main/java/com/example/questionnare/MainActivity.java
+++ app/src/main/java/com/example/questionnare/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText userEmailET;
     private EditText userPasswordET;
-    private Button LoginButton;
-    private TextView signInRedirect;
     private DatabaseManager dbManager; // DatabaseManager példánya
 
     @Override
@@ -26,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
         userEmailET = findViewById(R.id.emailLogin);
         userPasswordET = findViewById(R.id.passwordLogin);
-        LoginButton = findViewById(R.id.loginButton);
-        signInRedirect = findViewById(R.id.signinRedirect);
+        Button loginButton = findViewById(R.id.loginButton);
+        TextView signInRedirect = findViewById(R.id.signinRedirect);
         dbManager = new DatabaseManager(this); // Inicializálás
         dbManager.open();
         dbManager.addAdmin();
@@ -41,36 +38,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         //ha minden jó akkor megnyitja a home képernyőt
-        LoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email =userEmailET.getText().toString();
-                String password = userPasswordET.getText().toString();
-                if (dbManager.checkUser(email, password)) {
-                    dbManager.close();
-                    if(email.equals("admin@gmail.com")){
-                        Toast.makeText(MainActivity.this, "Successfully logged in as the admin", Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(MainActivity.this, "Successfully logged in as "+email, Toast.LENGTH_SHORT).show();
-                    }
-                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                    intent.putExtra("email", email);
-                    intent.putExtra("password", password);
-                    startActivity(intent);
+        loginButton.setOnClickListener(view -> {
+            String email =userEmailET.getText().toString();
+            String password = userPasswordET.getText().toString();
+            if (dbManager.checkUser(email, password)) {
+                dbManager.close();
+                if(email.equals("admin@gmail.com")){
+                    Toast.makeText(MainActivity.this, "Successfully logged in as the admin", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(MainActivity.this, "Invalid e-mail/password combination", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Successfully logged in as "+email, Toast.LENGTH_SHORT).show();
                 }
-
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+                startActivity(intent);
+            }else{
+                Toast.makeText(MainActivity.this, "Invalid e-mail/password combination", Toast.LENGTH_SHORT).show();
             }
+
         });
 
         //átirányitás a regisztráció képernyőre
-        signInRedirect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
+        signInRedirect.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
         });
 
 
