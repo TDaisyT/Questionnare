e1895fb0bf2585f8b3ad461c23a373b5df2fe792
FktEniko <feniko78@gmail.com>
2023-12-03 02:53:07 +0100

    lambdabetétel SignUpActivity.java

--- app/src/main/java/com/example/questionnare/SignUpActivity.java
+++ app/src/main/java/com/example/questionnare/SignUpActivity.java
@@ -2,7 +2,6 @@ package com.example.questionnare;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -20,8 +19,6 @@ public class SignUpActivity  extends AppCompatActivity {
     private EditText passwordSignin;
     private EditText passwordAgain;
     private CheckBox checkBox;
-    private Button singUpButton;
-    private TextView loginRedirect;
     private DatabaseManager dbManager;
     private boolean validEmail=true;
     @Override
@@ -34,51 +31,45 @@ public class SignUpActivity  extends AppCompatActivity {
         passwordSignin = findViewById(R.id.passwordSignUp);
         passwordAgain = findViewById(R.id.passwordAgain);
         checkBox = findViewById(R.id.checkBox);
-        singUpButton = findViewById(R.id.signUpButton);
-        loginRedirect = findViewById(R.id.loginRedirect);
+        Button singUpButton = findViewById(R.id.signUpButton);
+        TextView loginRedirect = findViewById(R.id.loginRedirect);
 
 
 
 
-        singUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email =emailSignin.getText().toString();
-                String password = passwordSignin.getText().toString();
-                String passwordagain = passwordAgain.getText().toString();
-                //ha már van az adatbázisban ilyen e-mail címre felhasználó, nem enged regisztrálni
-                if(dbManager.checkEmail(email)){
-                    Toast.makeText(SignUpActivity.this, "This account already exists", Toast.LENGTH_SHORT).show();
-                    validEmail=false;
-                }
-                //ha nem egyezik a két jelszó, nem enged regisztrálni
-                if(!password.equals(passwordagain)){
-                    Toast.makeText(SignUpActivity.this, "You entered different passwords", Toast.LENGTH_SHORT).show();
-                }
-                //ha nincs bepipálva, nem enged regisztrálni
-                if(!checkBox.isChecked()){
-                    Toast.makeText(SignUpActivity.this, "You have to accept the terms of use", Toast.LENGTH_SHORT).show();
-                }
-                //csak akkor dob vissza a bejelentkező képernyőre ha megfelelőek az adatok
-                //rögzíti az adatokat az adatbázisba
-                if(validEmail && password.equals(passwordagain) && checkBox.isChecked()){
-                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                    Toast.makeText(SignUpActivity.this, "Account creation was successful", Toast.LENGTH_SHORT).show();
-                    dbManager.addUser(email,password);
-                    dbManager.close();
-                    startActivity(intent);
-                }
-
+        singUpButton.setOnClickListener(view -> {
+            String email =emailSignin.getText().toString();
+            String password = passwordSignin.getText().toString();
+            String passwordagain = passwordAgain.getText().toString();
+            //ha már van az adatbázisban ilyen e-mail címre felhasználó, nem enged regisztrálni
+            if(dbManager.checkEmail(email)){
+                Toast.makeText(SignUpActivity.this, "This account already exists", Toast.LENGTH_SHORT).show();
+                validEmail=false;
             }
+            //ha nem egyezik a két jelszó, nem enged regisztrálni
+            if(!password.equals(passwordagain)){
+                Toast.makeText(SignUpActivity.this, "You entered different passwords", Toast.LENGTH_SHORT).show();
+            }
+            //ha nincs bepipálva, nem enged regisztrálni
+            if(!checkBox.isChecked()){
+                Toast.makeText(SignUpActivity.this, "You have to accept the terms of use", Toast.LENGTH_SHORT).show();
+            }
+            //csak akkor dob vissza a bejelentkező képernyőre ha megfelelőek az adatok
+            //rögzíti az adatokat az adatbázisba
+            if(validEmail && password.equals(passwordagain) && checkBox.isChecked()){
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                Toast.makeText(SignUpActivity.this, "Account creation was successful", Toast.LENGTH_SHORT).show();
+                dbManager.addUser(email,password);
+                dbManager.close();
+                startActivity(intent);
+            }
+
         });
 
         // vissza visz a bejelentkezéshez
-        loginRedirect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                    startActivity(intent);
-            }
+        loginRedirect.setOnClickListener(view -> {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
         });
     }
 
